9a7bc7e88b670cc1da741fff341ac789d5d98d17
Akino <akinochen@foxmail.com>
2024-05-17 17:32:24 +0800

    chore: remove unused deps

--- vite.config.ts
+++ vite.config.ts
@@ -17,21 +17,9 @@ export default defineConfig({
     },
     extensions: ['.mjs', '.js', '.ts'],
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:4523/m2/3871056-3505026-default/171999476',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
   plugins: [dts()],
   test: {
     clearMocks: true,
-    environment: 'jsdom',
     reporters: ['default'],
     coverage: {
       reporter: ['text', 'json-summary', 'json'],
