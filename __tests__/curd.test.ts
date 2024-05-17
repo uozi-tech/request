307d3260fd39f212061df38b418f10a4dd39437f
Akino <akinochen@foxmail.com>
2024-05-17 17:35:26 +0800

    test: add test file

---
+++ __tests__/curd.test.ts
@@ -0,0 +1,11 @@
+import { describe, it } from 'vitest'
+// import Curd from '../core'
+
+describe('curd', () => {
+  it('test', async () => {
+    // Curd.setPagination()
+    // const api = new Curd('http://127.0.0.1:4523/m2/3871056-3505026-default/171999476')
+    // const res = await api.getList({ a: 1 })
+    // console.log(res)
+  })
+})
